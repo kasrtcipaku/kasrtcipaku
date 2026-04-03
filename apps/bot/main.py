c942f2d1539380e6055d9f3fa64bf5b1b429400b
Suarasa Policy Lab <suarasa.policylab@gmail.com>
2026-04-03 12:10:24 +0700

    fix

--- apps/bot/main.py
+++ apps/bot/main.py
@@ -12,7 +12,7 @@ from telegram.ext import (
     Application, CommandHandler, MessageHandler,
     CallbackQueryHandler, filters
 )
-from handlers.start import cmd_start, cmd_saldo, cmd_help, cmd_hubungkan, cmd_lunas
+from handlers.start import cmd_start, cmd_saldo, cmd_help, cmd_hubungkan, cmd_lunas, cmd_putuskan
 from handlers.messages import handle_message
 from handlers.callbacks import handle_callback
 from report import send_monthly_report
@@ -49,6 +49,7 @@ app_bot.add_handler(CommandHandler("saldo",     cmd_saldo))
 app_bot.add_handler(CommandHandler("help",      cmd_help))
 app_bot.add_handler(CommandHandler("hubungkan", cmd_hubungkan))
 app_bot.add_handler(CommandHandler("lunas",     cmd_lunas))
+app_bot.add_handler(CommandHandler("putuskan",  cmd_putuskan))
 app_bot.add_handler(CallbackQueryHandler(handle_callback))
 app_bot.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
